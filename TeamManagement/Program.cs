6c5c484db8ce945cfdc2af272372e98503ee2068
aforamitdev <aforamitrai@gmail.com>
2023-10-09 00:20:26 +0530

    Initial commit

---
+++ TeamManagement/Program.cs
@@ -0,0 +1,41 @@
+using Microsoft.AspNetCore.HttpOverrides;
+using TeamManagement.Extensions;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCors();
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}else
+{
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseForwardedHeaders(new ForwardedHeadersOptions()
+{
+    ForwardedHeaders = ForwardedHeaders.All
+});
+
+app.UseCors("CorsPolicy");
+
+
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
