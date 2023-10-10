48cab26cce5719e8c1128872b885e6bddf98d277
aforamitdev <aforamitrai@gmail.com>
2023-10-10 23:55:41 +0530

    added entities and repository

---
+++ Respository/RepositoryContext.cs
@@ -0,0 +1,16 @@
+﻿using Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Respository;
+
+public class RepositoryContext:DbContext
+{
+    public RepositoryContext(DbContextOptions options):base(options)
+    {
+        
+    }
+
+    public DbSet<Company>? Companies { get; set; }
+    public DbSet<Employee>? Employees { get; set; }
+
+}
