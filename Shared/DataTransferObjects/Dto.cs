ebf1287bad14f0943ada7001f8529145f889b693
aforamitdev <aforamitrai@gmail.com>
2023-11-01 00:35:19 +0530

    0.

--- Shared/DataTransferObjects/Dto.cs
+++ Shared/DataTransferObjects/Dto.cs
@@ -1,5 +1,12 @@
 namespace Shared.DataTransferObjects;
 
-public record CompanyDto(Guid Id, string Name, string FullAddress);
+public record CompanyDto
+{
+    public Guid Id { get; set; }
+    public string? Name { get; set; }
+    public string FullAddress { get; set; }
+
+
+}
 
 public record EmployeeDto(Guid Id,string Name,int Age,string Position);
